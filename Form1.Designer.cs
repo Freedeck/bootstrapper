d22e9274ed5075e916e461a4fc150f0945cc09ed
Aiden <aidenjs9@icloud.com>
2024-04-27 09:44:18 -0500

    GIT I LOVE GIT I LOE GIT + A redesign because this is now the installer

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,34 +30,101 @@
         {
             label1 = new Label();
             progressBar1 = new ProgressBar();
+            label2 = new Label();
+            button1 = new Button();
+            folderBrowserDialog1 = new FolderBrowserDialog();
+            textBox1 = new TextBox();
+            button2 = new Button();
+            label3 = new Label();
+            richTextBox1 = new RichTextBox();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
             label1.Location = new Point(10, 10);
             label1.Name = "label1";
-            label1.Size = new Size(162, 15);
+            label1.Size = new Size(110, 32);
             label1.TabIndex = 0;
-            label1.Text = "Downloading required assets.";
+            label1.Text = "Freedeck";
             // 
             // progressBar1
             // 
-            progressBar1.Location = new Point(10, 28);
+            progressBar1.Location = new Point(92, 212);
             progressBar1.Name = "progressBar1";
-            progressBar1.Size = new Size(232, 23);
+            progressBar1.Size = new Size(370, 23);
             progressBar1.TabIndex = 1;
             progressBar1.Click += progressBar1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(12, 42);
+            label2.Name = "label2";
+            label2.Size = new Size(450, 15);
+            label2.TabIndex = 2;
+            label2.Text = "You're one click away from installing the FOSS alternative to the Elgato Stream Deck.";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(10, 212);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 3;
+            button1.Text = "Install!";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(118, 60);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(253, 23);
+            textBox1.TabIndex = 4;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(377, 60);
+            button2.Name = "button2";
+            button2.Size = new Size(38, 23);
+            button2.TabIndex = 5;
+            button2.Text = "...";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(20, 68);
+            label3.Name = "label3";
+            label3.Size = new Size(92, 15);
+            label3.TabIndex = 6;
+            label3.Text = "Installation Path";
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(92, 142);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(370, 64);
+            richTextBox1.TabIndex = 7;
+            richTextBox1.Text = "";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(254, 71);
+            ClientSize = new Size(478, 247);
+            Controls.Add(richTextBox1);
+            Controls.Add(label3);
+            Controls.Add(button2);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Controls.Add(label2);
             Controls.Add(progressBar1);
             Controls.Add(label1);
             Name = "Form1";
-            Text = "Freedeck Bootstrapper";
+            Text = "Freedeck Installer";
             Load += Form1_Load;
             Shown += Form1_Ready;
             ResumeLayout(false);
@@ -68,5 +135,12 @@
 
         private Label label1;
         private ProgressBar progressBar1;
+        private Label label2;
+        private Button button1;
+        private FolderBrowserDialog folderBrowserDialog1;
+        private TextBox textBox1;
+        private Button button2;
+        private Label label3;
+        private RichTextBox richTextBox1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -10,19 +10,23 @@ namespace FreedeckLauncher
             InitializeComponent();
         }
 
-String folder = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        String folder = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
         private void Form1_Load(object sender, EventArgs e)
         {
-            WebClient wc = new WebClient();
-            wc.DownloadFileTaskAsync("https://freedeck.app/installer.zip", folder+"\\fdtemp.zip");
-            wc.DownloadProgressChanged += Wc_DownloadProgressChanged;
-            wc.DownloadFileCompleted += Wc_DownloadFileCompleted;
+            richTextBox1.Text += "Welcome to the installer!";
         }
 
         private void Wc_DownloadFileCompleted(object? sender, System.ComponentModel.AsyncCompletedEventArgs e)
         {
-            ZipFile.ExtractToDirectory(folder + "\\fdtemp.zip", folder+"\\fdt");
-            System.Diagnostics.Process.Start(folder+"\\fdt\\installer.exe");
+            try
+            {
+                ZipFile.ExtractToDirectory(folder + "\\fdtemp.zip", folder + "\\fdt");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("error while trying to extract: " + ex);
+            }
+            System.Diagnostics.Process.Start(folder + "\\fdt\\installer-win32-x64\\installer.exe");
         }
 
         private void Wc_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
@@ -38,6 +42,22 @@ String folder = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDoc
         private void progressBar1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += "Initializing install";
+            richTextBox1.Text += "Checking for git..";
+            if (!Directory.Exists("C:\\Program Files\\Git\\bin"))
+            {
+                richTextBox1.Text += "Couldn't find git; opening bootstrapper!";
+                GitBootstrap bs = new GitBootstrap();
+                bs.ShowDialog();
+            }
+            WebClient wc = new WebClient();
+            wc.DownloadFileTaskAsync("https://freedeck.app/installer.zip", folder + "\\fdtemp.zip");
+            wc.DownloadProgressChanged += Wc_DownloadProgressChanged;
+            wc.DownloadFileCompleted += Wc_DownloadFileCompleted;
         }
     }
 }
